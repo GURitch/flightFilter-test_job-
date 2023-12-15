233c1065ebcb8e2fdad54a164593422b35d429cd
Arthur <guritch21@gmail.com>
2023-12-15 16:14:15 +0300

    added javadoc

--- src/com/gridnine/testing/FlightFilterImpl.java
+++ src/com/gridnine/testing/FlightFilterImpl.java
@@ -6,6 +6,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FlightFilterImpl implements FlightFilter {
+    /**
+     * Filters flights with departure dates before the current moment.
+     *
+     * @param flights The list of flights to filter.
+     * @return A list of flights with departure dates after the current moment.
+     */
     @Override
     public List<Flight> filteringByDepartureBeforeNow(List<Flight> flights) {
         LocalDateTime now = LocalDateTime.now();
@@ -16,6 +22,12 @@ public class FlightFilterImpl implements FlightFilter {
     }
 
     //тут можно не фильтровать а выбрасывать исключение, в зависимости от задачи
+    /**
+     * Filters flights with arrival dates before their respective departure dates.
+     *
+     * @param flights The list of flights to filter.
+     * @return A list of flights with arrival dates after their respective departure dates.
+     */
     @Override
     public List<Flight> filteringByArrivalBeforeDeparture(List<Flight> flights) {
         return flights.stream()
@@ -24,6 +36,13 @@ public class FlightFilterImpl implements FlightFilter {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters flights with total ground time exceeding the specified number of hours.
+     *
+     * @param flights The list of flights to filter.
+     * @param hours   The maximum allowed ground time in hours.
+     * @return A list of flights with ground time less than the specified number of hours.
+     */
     @Override
     public List<Flight> filteringByTimeOnGround(List<Flight> flights, int hours) {
         return flights.stream()
@@ -31,7 +50,13 @@ public class FlightFilterImpl implements FlightFilter {
                 .collect(Collectors.toList());
     }
 
-    //    Считаем сумму часов на земле в одном перелете
+    /**
+     * Calculates the total ground time in hours for a list of flight segments.
+     *
+     * @param segments The list of flight segments.
+     * @return The total ground time in hours.
+     * @throws IncorrectDateBetweenSegmentsException if there is an issue with the dates between segments.
+     */
     private int calculateGroundTime(List<Segment> segments) {
         int groundTime = 0;
         for (int i = 1; i < segments.size(); i++) {
@@ -44,7 +69,13 @@ public class FlightFilterImpl implements FlightFilter {
         return groundTime;
     }
 
-    //    проверяем что дата отправления у следующего сегмента не раньше даты прибытия в предыдущем
+    /**
+     * Validates that the departure date of each segment is before the arrival date of the next segment.
+     *
+     * @param segments The list of flight segments to validate.
+     * @return The list of flight segments if validation passes.
+     * @throws IncorrectDateBetweenSegmentsException if there is an issue with the dates between segments.
+     */
     private List<Segment> validateByDateBetweenSegments(List<Segment> segments) {
         for (int i = 1; i < segments.size(); i++) {
             if (segments.get(i - 1).getArrivalDate().isAfter(segments.get(i).getDepartureDate())) {
