3a57684d92de666ec4c902399b06a861d7c00afc
Arthur <guritch21@gmail.com>
2023-12-17 16:08:14 +0300

    added checks for null and empty

--- src/com/gridnine/testing/FlightFilterImplTest.java
+++ src/com/gridnine/testing/FlightFilterImplTest.java
@@ -9,37 +9,35 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlightFilterImplTest {
+    private final FlightFilter out = new FlightFilterImpl();
 
     @Test
-    void filteringByDepartureBeforeNow() {
-        Segment segment = new Segment(LocalDateTime.now().minusDays(3), LocalDateTime.now().plusDays(3));
+    void filteringByDepartureBeforeNowTest() {
+        Segment segment = new Segment(LocalDateTime.now(), LocalDateTime.now().plusHours(3));
         Flight flight = new Flight(new ArrayList<>(List.of(segment)));
         List<Flight> flights = new ArrayList<>();
         flights.add(flight);
 
-        FlightFilter flightFilter = new FlightFilterImpl();
-
-        List<Flight> result = flightFilter.filteringByDepartureBeforeNow(flights);
+        List<Flight> result = out.filteringByDepartureBeforeNow(flights);
 
         assertEquals(0, result.size());
     }
 
     @Test
-    void filteringByArrivalBeforeDeparture() {
-        Segment segment = new Segment(LocalDateTime.now(), LocalDateTime.now().minusDays(3));
+    void filteringByArrivalBeforeDepartureTest() {
+        Segment segment = new Segment(LocalDateTime.now(), LocalDateTime.now().minusHours(3));
         Flight flight = new Flight(new ArrayList<>(List.of(segment)));
         List<Flight> flights = new ArrayList<>();
         flights.add(flight);
 
-        FlightFilter flightFilter = new FlightFilterImpl();
-
-        List<Flight> result = flightFilter.filteringByArrivalBeforeDeparture(flights);
+        List<Flight> result = out.filteringByArrivalBeforeDeparture(flights);
 
         assertEquals(0, result.size());
     }
 
+
     @Test
-    void filteringByTimeOnGround() {
+    void filteringByTimeOnGroundTest() {
         Segment segment1 = new Segment(LocalDateTime.now(), LocalDateTime.now().plusHours(1));
         Segment segment2 = new Segment(LocalDateTime.now().plusHours(4), LocalDateTime.now().plusHours(5));
         Flight flight = new Flight(new ArrayList<>(List.of(segment1, segment2)));
@@ -51,5 +49,35 @@ class FlightFilterImplTest {
         List<Flight> result = flightFilter.filteringByTimeOnGround(flights, hoursOnGround);
 
         assertEquals(0, result.size());
+    }
+    @Test
+    void exceptionTest(){
+        Segment segment1 = new Segment(LocalDateTime.now(), LocalDateTime.now().plusHours(3));
+        Segment segment2 = new Segment(LocalDateTime.now(), LocalDateTime.now().plusHours(3));
+
+        List<Segment> segments = new ArrayList<>();
+        segments.add(segment1);
+        segments.add(segment2);
+
+        Flight flight1 = new Flight(new ArrayList<>(segments));
+        List<Flight> flights1 = new ArrayList<>();
+        flights1.add(flight1);
+
+        assertThrows(IncorrectDateBetweenSegmentsException.class, ()-> out.filteringByDepartureBeforeNow(flights1));
+
+        Segment segment3 = null;
+        segments.add(1,segment3);
+        Flight flight2 = new Flight(new ArrayList<>(segments));
+        List<Flight> flights2 = new ArrayList<>();
+        flights2.add(flight2);
+
+        assertThrows(IllegalArgumentException.class, ()-> out.filteringByArrivalBeforeDeparture(flights2));
+
+        Flight flight3 = null;
+        List<Flight> flights3 = new ArrayList<>();
+        flights3.add(flight3);
+
+        assertThrows(IllegalArgumentException.class, ()-> out.filteringByArrivalBeforeDeparture(flights3));
+
     }
 }
